d11e49e33c6c6d49bd53b050223b472e3b09a4f5
Pain31415 <Cherniy412@ukr.net>
2023-09-10 11:08:21 +0200

    ex 1 was done

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('Hello World')
+import math
+
+a=float(input('Enter a:'))
+b=float(input('Enter b '))
+print(f'Sum = {a+b}')
+print(f'Sub = {a-b}')
+print(f'Mul = {a*b}')
+print(f'Div = {a/b}')
